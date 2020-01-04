906f217bbb873a6817a8f95a8d1d6d76310442ed
Miguel Luiz Martins <miguellm@gmail.com>
2020-01-04 16:56:49 -0300

    Add files via upload
    Arquivos da versão inicial do projeto em C#

---
+++ ExemploWebAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,28 @@
+﻿using ExemploWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private static List<Produto> produtos = new List<Produto>();
+
+        [HttpGet]
+        public List<Produto> Get()
+        {
+            return produtos;
+        }
+        public void Post (string nome, string quantidade, string preco )
+        {
+
+            produtos.Add(new Produto(nome, quantidade, preco));
+        }
+      }
+}
---
+++ ExemploWebAPI/Models/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploWebAPI.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public string Quantidade { get; set; }
+
+        public string Preco { get; set; }
+        public Produto( string texto, string quantidade, string preco)
+        {
+            this.Nome = texto;
+            this.Quantidade = quantidade;
+            this.Preco = preco;
+        }
+    }
+}
